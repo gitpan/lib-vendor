66153b3815776e8895256bbd5978fc40d6a7690d
Bob Kleemann <bobk@cpan.org>
2014-05-14 00:11:57 +0000

    Import of BOBK/lib-vendor-0.14.134 from CPAN.
    gitpan-cpan-distribution: lib-vendor
    gitpan-cpan-version:      0.14.134
    gitpan-cpan-path:         BOBK/lib-vendor-0.14.134.tar.gz
    gitpan-cpan-author:       BOBK
    gitpan-cpan-maturity:     released

--- lib/lib/vendor.pm
+++ lib/lib/vendor.pm
@@ -1,8 +1,8 @@
 package lib::vendor;
+$lib::vendor::VERSION = '0.14.134';
 
 use strict;
 
-use Cwd         ();
 use FindBin     ();
 use File::Spec  ();
 
@@ -22,24 +22,39 @@ use File::Spec  ();
 
 our $APPDIR;
 BEGIN {
-    ( $APPDIR = $FindBin::RealBin ) =~ s!/bin$!! unless $APPDIR;
+    if ( not $APPDIR ) {
+        $APPDIR = $FindBin::RealBin;
+        while ( $APPDIR ne '/' && !-d File::Spec->catdir( $APPDIR, 'lib' ) ) {
+            # Search upwards in the directory structure
+            # until we find a subdirectory named 'lib'.
+            my @appdir = File::Spec->splitdir($APPDIR);
+            pop @appdir;
+            $APPDIR = File::Spec->catdir(@appdir);
+        }
+    }
 }
 
+our $VENDOR //= 'vendor';
 sub import {
     my ( $package, @vendors ) = @_;
 
-    for my $vendor (@vendors) {
-        $vendor = Cwd::abs_path(
-            File::Spec->catdir( $APPDIR, "vendor/$vendor/lib" )
-        );
+    if ( @vendors ) {
+        if ( defined $vendors[0] && $vendors[0] eq '-vendor' ) {
+            ( undef, my $vendor, @vendors ) = @vendors;
+            $VENDOR = $vendor if defined $vendor;
+        }
+
+        for my $vendor (@vendors) {
+            $vendor = File::Spec->catdir( $APPDIR, $VENDOR, $vendor, 'lib' );
+        }
     }
-    unshift @vendors,
-        Cwd::abs_path( File::Spec->catdir( $APPDIR, "lib" ) );
+    unshift @vendors, File::Spec->catdir( $APPDIR, "lib" );
 
     shrink_INC(@vendors);
 }
 
 sub shrink_INC {
+    local $_;
     my %seen = ();
     @INC = grep {
         my $key;
@@ -63,23 +78,27 @@ __END__
 
 =head1 NAME
 
-lib::vendor - add vendor libraries to the module search path
+lib::vendor - add vendor libraries to the module search path (@INC)
+
+=head1 VERSION
+
+version 0.14.134
 
 =head1 SYNOPSIS
 
-  # Include only $FindBin::RealBin/../lib in module search path.
+  # Include only $FindBin::RealBin/../lib in module search path (@INC).
   use lib::vendor;
 
 or
 
-  # Include in module search path:
+  # Include in module search path (@INC):
   # $FindBin::RealBin/../lib,
   # $FindBin::RealBin/../vendor/core/lib 
   use lib::vendor qw(core);
 
 or
 
-  # Include in module search path:
+  # Include in module search path (@INC):
   # $FindBin::RealBin/../lib,
   # $FindBin::RealBin/../vendor/core/lib,
   # $FindBin::RealBin/../vendor/common/lib,
@@ -97,6 +116,26 @@ or
 Locates the full path to the script home and adds its lib directory to the
 library search path, plus any vendor library directories specified.
 
+The script home is the directory closest to the original script which has
+a C<lib> subdirectory.  It first searches the directory that the script
+was executed from, then upwards until it finds a directory containing a
+C<lib> subdirectory.
+
+There is an optional configuration value of C<-vendor>, which will configure
+what the vendor directory is.
+
+  use lib::vendor -vendor => 'include', qw( this that );
+  # Include in module search path (@INC):
+  # $FindBin::RealBin/../lib,
+  # $FindBin::RealBin/../include/this,
+  # $FindBin::RealBin/../include/that
+
+  use lib::vendor -vendor => '', qw( this that );
+  # Include in module search path (@INC):
+  # $FindBin::RealBin/../lib,
+  # $FindBin::RealBin/../this,
+  # $FindBin::RealBin/../that
+
 =head1 AUTHOR
 
 Bob Kleemann
@@ -106,4 +145,3 @@ Bob Kleemann
 L<lib>, L<mylib>, L<FindBin>
 
 =cut
-
